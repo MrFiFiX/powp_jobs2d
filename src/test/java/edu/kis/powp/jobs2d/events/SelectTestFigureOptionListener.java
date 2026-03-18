7d60dfd6b74a19a59f7a0f518388eca5e69aa6dc
Filip C <252788@edu.p.lodz.pl>
2026-03-18 15:08:59 +0100

    Refactor figure selection to use enum with name lookup

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,13 +2,38 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.function.Consumer;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.AbstractDriverAdapter;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
+
+	private enum FigureUser {
+		JOE1("Figure Joe 1", d -> FiguresJoe.figureScript1(d.getCurrentDriver())),
+		JOE2("Figure Joe 2",d -> FiguresJoe.figureScript2(d.getCurrentDriver())),
+		JANE("Figure Jane", d -> FiguresJane.figureScript(new AbstractDriverAdapter(0,0,d.getCurrentDriver())));
+
+		String name;
+		Consumer<DriverManager> consumer;
+
+		public static FigureUser fromName(String name) {
+			for (FigureUser f : values()) {
+				if (f.name.equals(name)) {
+					return f;
+				}
+			}
+			return null;
+		}
+
+		private FigureUser(String s, Consumer<DriverManager> consumer){
+			name = s;
+			this.consumer = consumer;
+		}
+	}
 
 	private DriverManager driverManager;
 
@@ -18,12 +43,6 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().equals("Figure Joe 1")) {
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		} else if (e.getActionCommand().equals("Figure Joe 2")) {
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		} else if (e.getActionCommand().equals("Figure Jane")) {
-			FiguresJane.figureScript(new AbstractDriverAdapter(0,0,driverManager.getCurrentDriver()));
-		}
+		FigureUser.fromName(e.getActionCommand()).consumer.accept(driverManager);
 	}
 }
